48cf9b06af6f70a79587a996bcd818f0f05d0f2f
kiaanonthehub <kiaanmaharaj7@gmail.com>
2021-06-03 01:01:37 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Domingo_Roof_Works.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
